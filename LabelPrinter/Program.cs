efce3c31a4f689e6dd8030a617b563f5401e542c
Giancarlo Rhodes <giancarlo.rhodes@gmail.com>
2019-02-06 15:36:27 -0600

    adding good comments

--- LabelPrinter/Program.cs
+++ LabelPrinter/Program.cs
@@ -1,19 +1,32 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace LabelPrinter
+﻿namespace LabelPrinter
 {
+
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// AUTHOR: Giancarlo Rhodes
+    /// DESCRIPTION: Program class for console project type
+    /// COMPANY: Onshore Outsourcing https://www.onshoreoutsourcing.com/
+    /// </summary>
     class Program
     {
+
+        /// <summary>
+        /// AUTHOR: Giancarlo Rhodes
+        /// DESCRIPTION: entry point method
+        /// COMPANY: Onshore Outsourcing https://www.onshoreoutsourcing.com/
+        /// </summary>
         static void Main(string[] args)
         {
 
 
 
-
         }
+
+
     }
 }
