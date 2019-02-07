7b122dbb15aa716d006558b45ff133171db6eae5
Giancarlo Rhodes <giancarlo.rhodes@gmail.com>
2019-02-07 07:14:57 -0600

    example print

--- LabelPrinter/Letter.cs
+++ LabelPrinter/Letter.cs
@@ -26,30 +26,32 @@ namespace LabelPrinter
         /// </summary>
         internal static void Printer()
         {
-            Console.WriteLine("----------------------------------------------------------------------------------------------------------------------------------------------------");
-            Console.WriteLine("|                                                                                                                                                  |");
-            Console.WriteLine("|                                                                                                                                                  |");
-            Console.WriteLine("|                                                                                                                                                  |");
-            Console.WriteLine("|                                                                                                                                                  |");
-            Console.WriteLine("|                                                                                                                                                  |");
-            Console.WriteLine("|                                                                                                                                                  |");
-            Console.WriteLine("|                                                                                                                                                  |");
-            Console.WriteLine("|                                                                                                                                                  |");
-            Console.WriteLine("|                                                                                                                                                  |");
-            Console.WriteLine("|                                                                                                                                                  |");
-            Console.WriteLine("|                                                                                                                                                  |");
-            Console.WriteLine("|                                                                                                                                                  |");
-            Console.WriteLine("|                                                                                                                                                  |");
-            Console.WriteLine("|                                                                                                                                                  |");
-            Console.WriteLine("|                                                                                                                                                  |");
-            Console.WriteLine("|                                                                                                                                                  |");
-            Console.WriteLine("|                                                                                                                                                  |");
-            Console.WriteLine("|                                                                                                                                                  |");
-            Console.WriteLine("|                                                                                                                                                  |");
-            Console.WriteLine("|                                                                                                                                                  |");
-            Console.WriteLine("|                                                                                                                                                  |");
-            Console.WriteLine("|                                                                                                                                                  |");
-            Console.WriteLine("----------------------------------------------------------------------------------------------------------------------------------------------------");
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine("            ------------------------------------------------------------------------------------------------------------------------------------------");
+            Console.WriteLine("            |                                                                                                                                        |");
+            Console.WriteLine("            |      Giancarlo and Kathy Rhodes                                                                                                        |");
+            Console.WriteLine("            |      4749 Sweetheart Lane                                                                                                              |");
+            Console.WriteLine("            |      Columbia, MO 65382                                                                                                                |");
+            Console.WriteLine("            |                                                                                                                                        |");
+            Console.WriteLine("            |                                                                                                                                        |");
+            Console.WriteLine("            |                                                                                                                                        |");
+            Console.WriteLine("            |                                                                                                                                        |");
+            Console.WriteLine("            |                                                                                                                                        |");
+            Console.WriteLine("            |                                                            Onshore Outsourcing                                                         |");
+            Console.WriteLine("            |                                                            902 N. Missouri Street                                                      |");
+            Console.WriteLine("            |                                                            Macon, MO 63552                                                             |");
+            Console.WriteLine("            |                                                                                                                                        |");
+            Console.WriteLine("            |                                                                                                                                        |");
+            Console.WriteLine("            |                                                                                                                                        |");
+            Console.WriteLine("            |                                                                                                                                        |");
+            Console.WriteLine("            |                                                                                                                                        |");
+            Console.WriteLine("            |                                                                                                                                        |");
+            Console.WriteLine("            |                                                                                                                                        |");
+            Console.WriteLine("            |                                                                                                                                        |");
+            Console.WriteLine("            |                                                                                                                                        |");
+            Console.WriteLine("            |                                                                                                                                        |");
+            Console.WriteLine("            ------------------------------------------------------------------------------------------------------------------------------------------");
         }
     }
 }
