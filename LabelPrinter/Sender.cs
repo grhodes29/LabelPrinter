e27a3d98e3dd029c23605d758e513a09777c817b
Giancarlo Rhodes <giancarlo.rhodes@gmail.com>
2019-02-06 18:17:29 -0600

    add a change

--- LabelPrinter/Sender.cs
+++ LabelPrinter/Sender.cs
@@ -11,7 +11,7 @@ namespace LabelPrinter
 
     /// <summary>
     /// AUTHOR: Giancarlo Rhodes
-    /// DESCRIPTION: 
+    /// DESCRIPTION: add description
     /// COMPANY: Onshore Outsourcing https://www.onshoreoutsourcing.com/
     /// </summary>
     public class Sender
